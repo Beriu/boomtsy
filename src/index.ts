0a005fb56a0ed26ecef2dad9d9f70d2c474f74a3
tiberiu <tiberiu@example.com>
2020-12-21 22:25:41 +0200

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello! waddup');
+import ytdl from 'ytdl-core';
+import {Readable} from "stream";
+
+const audio: Readable = ytdl(
+    'https://www.youtube.com/watch?v=5gHdZkavGJk',
+    { quality: 'highestaudio', filter: 'audioonly' }
+    );
+
+/**
+ * TODO: Create broadcast
+ * https://github.com/fent/node-ytdl-core
+ * https://discord.js.org/#/docs/main/stable/class/Client
+ */
